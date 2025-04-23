cd469e27513ac1fa450372d784f55d4f543413d1
jeffersonrizzetto@gmail.com <jeffersonrizzetto@gmail.com>
2025-04-23 12:41:21 -0300

    Projeto Quiz de Programação finalizado

--- 10_QUIZ_PROGRAMACAO/quiz/src/App.jsx
+++ 10_QUIZ_PROGRAMACAO/quiz/src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
 import Welcome from "./components/Welcome";
+import { useContext, useEffect } from "react";
+import Question from "./components/Question";
+import { QuizContext } from "./context/quiz";
+import GameOver from "./components/GameOver";
+import PickCategory from "./components/PickCategory";
 
 function App() {
+  const [quizState, dispath] = useContext(QuizContext);
+
   return (
     <div className="App">
       <h1>Quiz de Programação</h1>
-      <Welcome />
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
+      {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
   );
 }
