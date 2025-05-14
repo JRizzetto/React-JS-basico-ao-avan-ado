b854c13f274d5227addb34caa92edc4557a8acb3
jeffersonrizzetto@gmail.com <jeffersonrizzetto@gmail.com>
2025-05-14 12:36:00 -0300

    Projeto Mini Blog

---
+++ 17_MINIBLOG/miniblog/src/firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNoy3vT8tQyqDR7cmrDqKhC8Dr4vFeW7g",
+  authDomain: "miniblog-3f892.firebaseapp.com",
+  projectId: "miniblog-3f892",
+  storageBucket: "miniblog-3f892.firebasestorage.app",
+  messagingSenderId: "869080804339",
+  appId: "1:869080804339:web:94d4eed946360f85f50086",
+  measurementId: "G-PP4H9TRWCD",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
